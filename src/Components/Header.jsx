31d57cff3def4ab2201295e0bcd2f236195843bf
NemanjaVukosavljevic <125715780+NemanjaVukosavljevic@users.noreply.github.com>
2024-05-15 17:58:39 +0200

    Added TeamMembers

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,6 +16,7 @@ const Header = () => {
                 <option value="Team A">Team A</option>
                 <option value="Team B">Team B</option>
                 <option value="Team C">Team C</option>
+                <option value="Team D">Team D</option>
             </select>
         </div>
         
--- src/Components/MembersSection.jsx
+++ src/Components/MembersSection.jsx
@@ -11,84 +11,84 @@ const MembersSection = () => {
             fullName: "Bob Jones",
             designation: "JavaScript Developer",
             gender: "male",
-            teamName: "TeamA"
+            teamName: "Team A"
           },
           {
             id: 2,
             fullName: "Jill Bailey",
             designation: "Node Developer",
             gender: "female",
-            teamName: "TeamA"
+            teamName: "Team A"
           },
           {
             id: 3,
             fullName: "Gail Shepherd",
             designation: "Java Developer",
             gender: "female",
-            teamName: "TeamA"
+            teamName: "Team A"
           },
           {
             id: 4,
             fullName: "Sam Reynolds",
             designation: "React Developer",
             gender: "male",
-            teamName: "TeamB"
+            teamName: "Team B"
           },
           {
             id: 5,
             fullName: "David Henry",
             designation: "DotNet Developer",
             gender: "male",
-            teamName: "TeamB"
+            teamName: "Team B"
           },
           {
             id: 6,
             fullName: "Sarah Blake",
             designation: "SQL Server DBA",
             gender: "female",
-            teamName: "TeamB"
+            teamName: "Team B"
           },
           {
             id: 7,
             fullName: "James Bennet",
             designation: "Angular Developer",
             gender: "male",
-            teamName: "TeamC"
+            teamName: "Team C"
           },
           {
             id: 8,
             fullName: "Jessica Faye",
             designation: "API Developer",
             gender: "female",
-            teamName: "TeamC"
+            teamName: "Team C"
           },
           {
             id: 9,
             fullName: "Lita Stone",
             designation: "C++ Developer",
             gender: "female",
-            teamName: "TeamC"
+            teamName: "Team C"
           },
           {
             id: 10,
             fullName: "Daniel Young",
             designation: "Python Developer",
             gender: "male",
-            teamName: "TeamD"
+            teamName: "Team D"
           },
           {
             id: 11,
             fullName: "Adrian Jacobs",
             designation: "Vue Developer",
             gender: "male",
-            teamName: "TeamD"
+            teamName: "Team D"
           },
           {
             id: 12,
             fullName: "Devin Monroe",
             designation: "Graphic Designer",
             gender: "male",
-            teamName: "TeamD"
+            teamName: "Team D"
           }
     ])
     

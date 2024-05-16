29945f51934460434199235b6ea80dc242f9a154
NemanjaVukosavljevic <125715780+NemanjaVukosavljevic@users.noreply.github.com>
2024-05-16 05:28:44 +0200

    react-router-dom vezbanje

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import FirstPage from './Components/FirstPage';
+import SecondPage from './Components/SecondPage';
 
 function App() {
   return (
-    <div className="react-router">
+    <>
+    <Router>
+      <div className="react-router bg-gray-500">
+        <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/FirstPage'>First Page</Link>
+            </li>
+            <li>
+              <Link to='/SecondPage'>Second Page</Link>
+            </li>
+        </ul>
+      </div>
 
-    </div>
+      <Routes>
+        <Route path='/'>
+
+        </Route>
+        <Route path='/FirstPage' element={<FirstPage />}>
+
+        </Route>
+        <Route path='/SecondPage' element={<SecondPage />}>
+
+        </Route>
+      </Routes>
+    </Router>
+    </>
+    
   );
 }
 
---
+++ src/Components/FirstPage.jsx
@@ -0,0 +1,12 @@
+const FirstPage = () => {
+
+    return (
+        <>
+            <div className="h-screen flex justify-center items-center">
+                <h1 className="text-5xl">PRVA STRANA</h1>
+            </div>
+        </>
+    )
+}
+
+export default FirstPage;
